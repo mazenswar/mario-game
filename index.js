6e72e0908407a1816085666ae5414ef690dacac7
Mazen Al Swar <43959520+mazenswar@users.noreply.github.com>
2023-03-14 19:57:04 -0400

    sprite

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const platformImg = "./img/platform.png";
 const backgroundImg = "./img/background.png";
 const hillsImg = "./img/hills.png";
+const spriteStandRight = "./img/stand-right.png";
+const spriteRunRight = "./img/spriteRunRight.png";
+const platformSmallTall = "./img/platformSmallTall.png";
+const run = "./img/run-2.png";
 const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
 const gravity = 0.5;
-const velocityRate = { x: 5, y: 15 };
 const movementRange = {
-	left: 100,
+	left: 10,
 	right: 500,
 };
-let scrollOffset = 0;
 const scrollRate = 5;
 canvas.width = 1024;
 canvas.height = 576;
@@ -24,31 +26,52 @@ function createImage(imgSrc) {
 ////////////// CLASSES
 class Player {
 	constructor() {
+		this.speed = 10;
+		this.jump = 15;
 		this.position = {
 			x: 100,
 			y: 100,
 		};
-		this.height = 30;
-		this.width = 30;
+		this.height = 120;
+		this.width = 120;
 		this.velocity = {
 			x: 0,
 			y: 1,
 		};
+		this.sprites = {};
+		this.currentSprite = createImage(run);
+		this.currentCropWidth = 120;
+		this.frames = 0;
 	}
 
 	draw() {
-		context.fillStyle = "red";
-		context.fillRect(this.position.x, this.position.y, this.width, this.height);
+		// context.fillStyle = "red";
+		// context.fillRect(this.position.x, this.position.y, this.width, this.height);
+		context.drawImage(
+			this.currentSprite,
+			this.currentCropWidth * this.frames,
+			0,
+			this.currentCropWidth,
+			120,
+			this.position.x,
+			this.position.y,
+			this.width,
+			this.height
+		);
 	}
 
 	update() {
+		this.frames++;
+		if (this.frames > 14) {
+			this.frames = 1;
+		}
 		this.draw();
 		this.position.x += this.velocity.x;
 		this.position.y += this.velocity.y;
 		if (this.height + this.position.y + this.velocity.y <= canvas.height) {
 			this.velocity.y += gravity;
 		} else {
-			this.velocity.y = 0;
+			// this.velocity.y = 0;
 		}
 	}
 }
@@ -85,16 +108,40 @@ class GenericObject {
 	}
 }
 //////////////////////// INIT
-const player = new Player();
-const platformImage = createImage(platformImg);
-const platforms = [
-	new Platform({ x: 0, y: 470, image: platformImage }),
-	new Platform({ x: platformImage.width - 2, y: 470, image: platformImage }),
-];
-const genericObjects = [
-	new GenericObject({ x: -1, y: -1, image: createImage(backgroundImg) }),
-	new GenericObject({ x: 0, y: 0, image: createImage(hillsImg) }),
-];
+let scrollOffset = 0;
+let player = new Player();
+let platformImage = createImage(platformImg);
+let platforms = [];
+let genericObjects = [];
+
+function init() {
+	scrollOffset = 0;
+	player = new Player();
+	platformImage = createImage(platformImg);
+	platforms = [
+		new Platform({ x: 0, y: 470, image: platformImage }),
+		new Platform({ x: platformImage.width - 2, y: 470, image: platformImage }),
+		new Platform({
+			x: platformImage.width * 2 + 300,
+			y: 470,
+			image: platformImage,
+		}),
+		new Platform({
+			x: platformImage.width * 3 + 900,
+			y: 470,
+			image: platformImage,
+		}),
+		new Platform({
+			x: platformImage.width * 5 + 500,
+			y: 250,
+			image: createImage(platformSmallTall),
+		}),
+	];
+	genericObjects = [
+		new GenericObject({ x: -1, y: -1, image: createImage(backgroundImg) }),
+		new GenericObject({ x: 0, y: 0, image: createImage(hillsImg) }),
+	];
+}
 
 const keys = {
 	right: {
@@ -118,26 +165,29 @@ function animate() {
 	});
 	player.update();
 	if (keys.right.pressed && player.position.x <= movementRange.right) {
-		player.velocity.x = velocityRate.x;
-	} else if (keys.left.pressed && player.position.x >= movementRange.left) {
-		player.velocity.x = velocityRate.x * -1;
+		player.velocity.x = player.speed;
+	} else if (
+		(keys.left.pressed && player.position.x >= movementRange.left) ||
+		(keys.left.pressed && scrollOffset > 0 && player.position.x > 0)
+	) {
+		player.velocity.x = player.speed * -1;
 	} else {
 		player.velocity.x = 0;
 		if (keys.right.pressed) {
 			scrollOffset += scrollRate;
 			platforms.forEach((platform) => {
-				platform.position.x -= velocityRate.x;
+				platform.position.x -= player.speed;
 			});
 			genericObjects.forEach((genericObject) => {
-				genericObject.position.x -= 3;
+				genericObject.position.x -= player.speed * 0.66;
 			});
-		} else if (keys.left.pressed) {
+		} else if (keys.left.pressed && scrollOffset > 0) {
 			scrollOffset -= scrollRate;
 			platforms.forEach((platform) => {
-				platform.position.x += velocityRate.x;
+				platform.position.x += player.speed;
 			});
 			genericObjects.forEach((genericObject) => {
-				genericObject.position.x += 3;
+				genericObject.position.x += player.speed * 0.66;
 			});
 		}
 	}
@@ -153,12 +203,18 @@ function animate() {
 			player.velocity.y = 0;
 		}
 	});
-
-	if (scrollOffset > 2000) {
+	// win condition
+	if (scrollOffset > 6000) {
 		console.log("Winner winner");
+	}
+	// lose condition
+	if (player.position.y > canvas.height) {
+		// reset
+		init();
 	}
 }
 
+init();
 animate();
 
 // EVENT LISTENERS
@@ -178,7 +234,9 @@ addEventListener("keydown", ({ key }) => {
 			break;
 		case "w":
 			console.log("up");
-			player.velocity.y -= velocityRate.y;
+			if (player.position.y > 0) {
+				player.velocity.y -= player.jump;
+			}
 			break;
 	}
 });
